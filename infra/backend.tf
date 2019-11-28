5fe68b0f587906d0da70e4f1ddc0974b485ee1c2
Ahmed Belgana <ahmed@hashicorp.com>
2019-11-28 11:27:04 -0500

    Remove Backend

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket  = "terraform-backend-store-test"
-        encrypt = true
-        key     = "terraform.tfstate"
-        region  = "eu-west-1"
-    }
-}
